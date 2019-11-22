b5011d215b2ff5b4f93496d9e66caac0fc8efda4
Nejc Kozlevčar <nejc.kozlevcar@solvera-lynx.com>
2019-11-22 15:37:50 +0100

    Standing template

--- sundayleagueApp/urls.py
+++ sundayleagueApp/urls.py
@@ -12,6 +12,7 @@ admin.autodiscover()
 urlpatterns = [
     path('', views.index, name='index'),
     url(r'^fixtures/(?P<league>[1-9]+)/$', views.fixtures, name='fixtures'),
+    url(r'^standing/(?P<league>[1-9]+)/$', views.standing, name='standing'),
     path('teams/', views.teams, name="teams-all"),
     path('results/', views.results, name="all-results"),
     path('resultstext/', views.results_text, name="all-resultstext"),
--- sundayleagueApp/views.py
+++ sundayleagueApp/views.py
@@ -5,7 +5,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import services.FixturesServices as FixturesServices
 import services.ResultsService as ResultsService
-from collections import defaultdict
+from django.shortcuts import redirect
+import datetime
 
 LEAGUE_PREFIX = 'league_'
 
@@ -17,15 +18,33 @@ def index(request):
 
 def fixtures(request, league):
     if request.method == 'GET':
+        selected_round = request.GET.get('round', None)
         all_rounds = Round.objects.filter(league_number=league)
+        if selected_round is None:
+            today = datetime.date.today()
+            today_plus_2 = today + datetime.timedelta(days=2)
+            last_round = all_rounds.order_by('date').filter(date__lte=today_plus_2).last()
+            return redirect(request.path + "?round=" + str(last_round.round_number))
+        filter_rounds = all_rounds
+        if selected_round != 'all':
+            filter_rounds = filter_rounds.filter(league_number=league, round_number=selected_round)
         rounds_group_by_league = {}
-        [rounds_group_by_league.setdefault(LEAGUE_PREFIX + str(r.league_number), []).append(r) for r in all_rounds]
+        [rounds_group_by_league.setdefault(LEAGUE_PREFIX + str(r.league_number), []).append(r) for r in filter_rounds]
         all_matches = Match.objects.all().order_by("time")
         matches_group_by_rounds = {}
         [matches_group_by_rounds.setdefault(m.round_id, []).append(m) for m in all_matches]
         table_rows = TableRow.objects.filter(league=league).order_by('-points').all()
         return render(request, 'fixtures.html',
-                      {'matches': matches_group_by_rounds, 'rounds': rounds_group_by_league, 'table_rows': table_rows})
+                      {'matches': matches_group_by_rounds, 'rounds': rounds_group_by_league, 'all_rounds': all_rounds,
+                       'table_rows': table_rows, 'fixtures_class': 'btn-light', 'standing_class': 'btn-secondary',
+                       'selected_round': selected_round})
+
+
+def standing(request, league):
+    if request.method == 'GET':
+        table_rows = TableRow.objects.filter(league=league).order_by('-points').all()
+        return render(request, 'standing.html',
+                      {'table_rows': table_rows, 'fixtures_class': 'btn-secondary', 'standing_class': 'btn-light'})
 
 
 # todo: basic auth
