7f5666ef897bd3a8930b199f45a7849bfa86612c
Nejc Kozlevčar <nejc.kozlevcar@solvera-lynx.com>
2020-03-01 19:43:09 +0100

    WIP

--- sundayleagueApp/admin.py
+++ sundayleagueApp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.utils.html import format_html
+from django.views.decorators.csrf import csrf_exempt
 
-from .views import results
+from services import ResultsService
 from .models import *
+from .views import results
 from django.conf.urls import url
 
 # Register your models here.
@@ -33,17 +36,28 @@ class FileAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
-            url(r'^resultstext/(?P<file_id>.+)/$', results, name='resultstext-by-id')
+            url(r'^results/(?P<file_id>.+)/$', self.admin_site.admin_view(self.process_results_action),
+                name='save_results_by_admin')
         ]
         return urls + custom_urls
 
     def file_actions(self, obj):
+        print(obj)
         # todo: add href
         return format_html(
-            '<a class="button" href="10">Uvozi</a>'
+            '<form action="results/{}/" method="post"><button type="submit"class="button">Uvozi</button></form>'.format(obj.id)
         )
     file_actions.short_description = "Uvozi datoteko"
 
-    def process_results_action(self, request, file_id):
-        if request.method != 'POST':
-            raise NotImplementedError()
+    @csrf_exempt
+    def process_results_action(self, request, file_id, *args, **kwargs):
+        print("lalalal")
+        if request.method == 'POST':
+            if int(file_id) > 0:
+                saved_results = ResultsService.save_results_for_file(file_id)
+                return HttpResponse("{} saved results for file: {}".format(len(saved_results), file_id))
+
+            ResultsService.save_results()
+            return HttpResponse("Save all results")
+        else:
+            return HttpResponse("Wrong method!", status=405)
